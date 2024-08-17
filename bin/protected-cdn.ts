3d5f9ddfb599a16d2c7b44cd805716454b22a3e0
Nick Gregory <nick.gregory@openenterprise.co.uk>
2024-08-17 15:52:20 +0100

    add tags

--- bin/protected-cdn.ts
+++ bin/protected-cdn.ts
@@ -19,7 +19,7 @@ const jwk_uri = process.env.JWKS_URI ?? "https://example1.com/path/to/jwks.json"
 const jwt_audience = process.env.AUDIENCE ?? "https://example1.com";
 const jwt_issuer = process.env.ISSUER ?? "6789,3456";
 
-new ProtectedCdnStack(app, capitalise(tenant) + capitalise(product) + capitalise(environment), {
+const stack = new ProtectedCdnStack(app, capitalise(tenant) + capitalise(product) + capitalise(environment), {
     tenant: tenant,
     environment: environment,
     product: product,
@@ -28,3 +28,7 @@ new ProtectedCdnStack(app, capitalise(tenant) + capitalise(product) + capitalise
     jwt_issuer: jwt_issuer,
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION ?? "eu-west-1" },
 });
+
+cdk.Tags.of(stack).add("tenant", tenant, {});
+cdk.Tags.of(stack).add("environment", environment, {});
+cdk.Tags.of(stack).add("product", product, {});
